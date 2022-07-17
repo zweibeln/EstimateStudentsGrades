c2e2695e0b69dbe080f0314c9a9c3734938b192b
unknown <utku.tahan.ee@gmail.com>
2022-07-17 09:28:33 +0200

    refactor main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from student_data_loader import load_student_data
 
 def __main__():
     # load the data
-    data = load_student_data()
+    data_train, data_val, data_test = load_student_data('mat')
 
 
 __main__()
