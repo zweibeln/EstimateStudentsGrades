a0c048ed4027ec2745d05e90f80be8d19a004a28
unknown <utku.tahan.ee@gmail.com>
2022-07-17 14:09:29 +0200

    add encoding features and test

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
 
 
 def visualize_correlation_of_features(data):
@@ -20,3 +21,26 @@ def check_missing_values(data):
         raise ValueError('Some of the data is missing')
     else:
         pass
+
+
+def encode_nonnumeric_values(data):
+    # get nonnumeric column names
+    nonnumeric_cloumns = [data.columns[index] for index,dtype in enumerate(data.dtypes) if dtype == 'object']
+
+    # before encoding make sure not a single value repeats itself in another column so make them uniue wrt columns
+    for name in nonnumeric_cloumns:
+        data[name] = data[name].apply(lambda x: name[0]+x)
+
+    # one hot encode the nonnumeric values
+    dummy_mtx = pd.DataFrame()
+    for name in nonnumeric_cloumns:
+        dummy_mtx = pd.concat([dummy_mtx, pd.get_dummies(data[name])], axis=1)
+
+    # merge the encoded values with the original data
+    data = pd.concat([data, dummy_mtx], axis=1)
+
+    # drop the nonnumeric values from data
+    for name in nonnumeric_cloumns:
+        data.drop([name], axis=1, inplace=True)
+
+    return data
--- test/test_data_prepocess.py
+++ test/test_data_prepocess.py
@@ -1,4 +1,4 @@
-from data_preprocess import check_missing_values
+from data_preprocess import check_missing_values, encode_nonnumeric_values
 import pandas as pd
 import pytest
 
@@ -11,3 +11,12 @@ def test_check_missing_values():
     df = pd.DataFrame(df, columns=['idx', 'letter', 'value', 'converted_tf'])
     assert pytest.raises(ValueError, check_missing_values, df)
 
+
+def test_encoding_features():
+    df = {'idx': [1, 2, 3, 4, 5],
+          'letter': ['a', 'b', 'c', 'd', 'e'],
+          'value': ['red', 'purple', 'blue', 'green', 'yellow']}
+    df = pd.DataFrame(df, columns=['idx', 'letter', 'value'])
+    df_encoded = encode_nonnumeric_values(df)
+    for dtype in df_encoded.dtypes:
+        assert dtype == 'int64' or dtype == 'uint8'
