8783f20074b87caf5f7021c588098bada4bf2b1e
unknown <utku.tahan.ee@gmail.com>
2022-07-17 13:02:34 +0200

    add sep=; to data_reader

--- student_data_loader.py
+++ student_data_loader.py
@@ -8,5 +8,5 @@ def load_student_data(lecture):
     """
     if lecture not in ['mat', 'por']:
         raise ValueError(f'Lecture {lecture} does not exist. Only mat or por is accepted as lecture')
-    return pd.read_csv(f'student-{lecture}.csv')
+    return pd.read_csv(f'student-{lecture}.csv', sep=';')
 
