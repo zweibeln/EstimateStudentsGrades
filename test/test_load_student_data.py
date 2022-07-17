0f437010bce7c7b6ad3b8693ff85cb26b8d84f58
unknown <utku.tahan.ee@gmail.com>
2022-07-17 09:27:33 +0200

    refactor test_load_student_data

--- test/test_load_student_data.py
+++ test/test_load_student_data.py
@@ -6,7 +6,11 @@ os.chdir('..')
 
 
 def test_load_student_data():
-    assert isinstance(load_student_data('mat'), list)
-    assert len(load_student_data('mat')) == 396
-    assert len(load_student_data('mat')[0][0]) == 227
+    assert isinstance(load_student_data('mat')[0], list)
+    assert (len(load_student_data('mat')[0]) + len(load_student_data('mat')[1]) +
+            len(load_student_data('mat')[2])) == 395  # Total length of full data
+    assert len(load_student_data('mat')[0][0]) == 33
+    assert len(load_student_data('mat')[0]) == 237  # Length of training set
+    assert len(load_student_data('mat')[1]) == 79  # Length of validation set
+    assert len(load_student_data('mat')[2]) == 79  # Length of test set
     assert pytest.raises(ValueError, load_student_data, 'cmb')
